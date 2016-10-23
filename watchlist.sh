2c2ec0406e72cd9548255e1a880fec53964e0e7b
Sean OHaver <spohaver@gmail.com>
2016-10-22 22:39:16 -0700

    making this more user readable

--- watchlist.sh
+++ watchlist.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+# This was created when I couldn't run the watch command
 
+# The loop will exit when a key is pressed
 while [ -z "$key" ]; do
     clear
     echo -e "This is a check of the $(pwd) directory.\n"
-    echo -e "There are $(ls -a | wc -l) lines.\n"
+    echo -e "There are $(ls -a | wc -l) lines in the directory.\n"
+    echo -e "Running 'df -h'"
     df -h
+    echo -e "\nRunning 'du -cksh'"
     du -cksh
     sleep 2
 
     read -t 1 -n 1 key
-#    if [[ -z "$key" ]]
-#    then
-#        break
-#    fi 
 done 
