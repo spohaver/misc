85fd57fed6d9a24a741ae6e958992c9452f461d0
Sean O'Haver <spohaver@gmail.com>
2018-11-17 21:02:50 -0900

    Adding minimal json output

--- python_scripts/json_outputter.py
+++ python_scripts/json_outputter.py
@@ -21,13 +21,29 @@ def optparser():
     usage = "usage: %prog [options]"
     parser = OptionParser(usage=usage)
     parser.add_option("-f", "--filename", help="json file")
-    parser.add_option("-i", "--indent", default=4,
-                      help="Number of spaces for indention")
-    parser.add_option("-s", "--sort", action="store_true",
-                      default=False, help="Sort the keys")
+    parser.add_option("-i", "--indent",
+            default=4,
+            help="Number of spaces for indention"
+            )
+    parser.add_option("-s", "--sort", 
+            action="store_true",
+            default=False,
+            help="Sort the keys"
+            )
+    parser.add_option("-m", "--minimal",
+            action="store_true",
+            default=False,
+            help="Use minimal separators to shorten whitespace (',',':')"
+            )
     (options, args) = parser.parse_args()
     if not options.filename and sys.stdin.isatty():
         parser.error("No JSON Data Found")
+    try:
+        int(options.indent)
+    except ValueError:
+        parser.error("{value} is not an integer!".format(
+            value=options.indent)
+            )
     return options
 
 def main():
@@ -37,7 +53,21 @@ def main():
     else:
         if not sys.stdin.isatty():
             json_data = json.load(sys.stdin)
-    print json.dumps(json_data,sort_keys=options.sort,indent=int(options.indent))
+    if options.minimal:
+        print("Indent Value {value} ignored..".format(
+            value=options.indent)
+            )
+        print json.dumps(
+                json_data,
+                separators=(',',':'),
+                sort_keys=options.sort,
+                )
+    else:
+        print json.dumps(
+                json_data,
+                sort_keys=options.sort,
+                indent=int(options.indent)
+                )
     return 0
 
 if __name__ == "__main__":
